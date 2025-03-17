16892f9019a46deefe2d62287bf10d9a5f1df52d
Tejaswi <tejaswi.naradasi@gmail.com>
2025-03-17 14:19:03 +0530

    Fixed Next.js errors in AddTransactionPage

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,34 +1,33 @@
-import React from "react";
 import { Suspense } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
 import CreateAccountDrawer from "@/components/create-account-drawer";
 import { getDashboardData, getUserAccounts } from "@/actions/dashboard";
-import { AccountCard } from "./_components/account-card";
-import  BudgetProgress  from "./_components/budget-progress";
 import { getCurrentBudget } from "@/actions/budget";
+import { AccountCard } from "./_components/account-card";
+import BudgetProgress from "./_components/budget-progress";
 import { DashboardOverview } from "./_components/transaction-overview";
-async function DashboardPage() {
-  const accounts = await getUserAccounts()
+
+export default async function DashboardPage() {
+  const accounts = await getUserAccounts(); 
   const defaultAccount = accounts?.find((account) => account.isDefault);
   let budgetData = null;
   if (defaultAccount) {
     budgetData = await getCurrentBudget(defaultAccount.id);
   }
   const transactions = await getDashboardData();
+
   return (
     <div className="space-y-8">
       {defaultAccount && (
-         <BudgetProgress
-         initialBudget={budgetData?.budget}
-         currentExpenses={budgetData?.currentExpenses || 0}
-       />
-      )}
-      <Suspense fallback={"Loading Overview..."}>
-        <DashboardOverview
-        accounts={accounts}
-        transactions={transactions || []}
+        <BudgetProgress
+          initialBudget={budgetData?.budget}
+          currentExpenses={budgetData?.currentExpenses || 0}
         />
+      )}
+
+      <Suspense fallback={"Loading Overview..."}>
+        <DashboardOverview accounts={accounts} transactions={transactions || []} />
       </Suspense>
 
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
@@ -41,12 +40,8 @@ async function DashboardPage() {
           </Card>
         </CreateAccountDrawer>
         {accounts.length > 0 &&
-          accounts?.map((account) => {
-            return <AccountCard key={account.id} account={account} />
-})}
+          accounts?.map((account) => <AccountCard key={account.id} account={account} />)}
       </div>
     </div>
   );
 }
-
-export default DashboardPage;
